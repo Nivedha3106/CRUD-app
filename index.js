5fe6502ddbe014dbd67aaf9d4e8fdba6aa2599d1
Nivedha3106 <nivedhamurugan8441@gmail.com>
2025-09-20 23:24:08 +0530

    ceated post and get users

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//variables
+let users = [];
+let idCounter = 1;
+
+// CREATE user
+app.post("/users", (req, res) => {
+  const { name, dob, aadhaar } = req.body;
+
+  if (!name || !dob || !aadhaar) {
+    return res.status(400).json({ error: "name, dob, and aadhaar are required" });
+  }
+
+  const newUser = {
+    id: idCounter++,
+    name,
+    dob,
+    aadhaar
+  };
+
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+// Get all the records
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
